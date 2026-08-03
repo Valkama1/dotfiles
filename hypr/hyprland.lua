c993505131c5e637d0c5c64cfe28267e8aeebfd5
Valkama <valkamafelix@proton.me>
2026-08-03 13:02:42 +0200

    feat: switch cursor to rose-pine

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -72,10 +72,10 @@ local menu = "qs -c noctalia-shell ipc call"
 
 -- See https://wiki.hypr.land/Configuring/Environment-variables/
 
-hl.env("XCURSOR_THEME", "XCursor-Pro-Dark")
+hl.env("XCURSOR_THEME", "BreezeX-RosePine-Linux")
 hl.env("XCURSOR_SIZE", 24)
 
-hl.env("HYPRCURSOR_THEME", "XCursor-Pro-Dark-Hyprcursor")
+hl.env("HYPRCURSOR_THEME", "rose-pine-hyprcursor")
 hl.env("HYPRCURSOR_SIZE", 24)
 
 --##################
@@ -618,6 +618,6 @@ hl.window_rule({
 
 -- Autostart
 hl.on("hyprland.start", function()
-    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'XCursor-Pro-Dark'")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'BreezeX-RosePine-Linux'")
     hl.exec_cmd("qs -c noctalia-shell")
 end)
